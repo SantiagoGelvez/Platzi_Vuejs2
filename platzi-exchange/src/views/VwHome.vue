ba046bded097fa6b7f4779d23f9a1d34195f3157
Santiago A. Gelvez <santiagogelvez29@gmail.com>
2022-09-17 00:12:08 -0500

    connect the app whit api of CoinCap

--- platzi-exchange/src/views/VwHome.vue
+++ platzi-exchange/src/views/VwHome.vue
@@ -1,12 +1,25 @@
 <template>
-    <px-assets-table />
+    <div>
+        <px-assets-table :assets="assets" />
+    </div>
 </template>
 
 <script>
 import PxAssetsTable from "@/components/PxAssetsTable.vue";
+import api from "@/api"
 
 export default {
     name: "VwHome",
     components: { PxAssetsTable },
+    
+    data() {
+        return {
+            assets: []
+        }
+    },
+
+    created() {
+        api.getAssets().then(assets => (this.assets = assets))
+    }
 };
 </script>
